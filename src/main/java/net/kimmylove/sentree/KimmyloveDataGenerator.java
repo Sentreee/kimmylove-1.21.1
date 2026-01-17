ad7206421783245497c58b6b6b5d65c3ea2bd1c7
Sentree <savageking2006810@gmail.com>
2026-01-17 07:22:12 -0500

    The start of something great

---
+++ src/main/java/net/kimmylove/sentree/KimmyloveDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kimmylove.sentree;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KimmyloveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
